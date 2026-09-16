5eaf6733cb005f4807027703088c8af66472f370
wajeht <58354193+wajeht@users.noreply.github.com>
2026-09-16 11:27:55 -0500

    fix(AppFooter): add heart emoji to footer

--- src/vue/components/ui/AppFooter.vue
+++ src/vue/components/ui/AppFooter.vue
@@ -4,7 +4,7 @@ const appVersion = __APP_VERSION__;
 
 <template>
   <footer class="text-center">
-    © 2026 · Made by
+    © 2026 · Made with ❤️ by
     <a
       class="underline-offset-4 hover:underline"
       href="https://github.com/wajeht"
